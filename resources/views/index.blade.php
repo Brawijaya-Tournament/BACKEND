46967129118c5d2421f46db56f7e0d305fde43a4
Bagas Raditya Nafi <bagasrnfull@gmail.com>
2022-08-24 17:00:57 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Brawijaya Tournament</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header class="header ">
+    <div class="content-header">
+        <img src="assets/img/Logo oke.svg" alt="" class="responsive">
+        <img src="assets/img/bt-logo.svg" alt="" class="responsive">
+    </div>
+    
+       
+  </header><!-- End #header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <img src="assets/img/comingsoon.png" alt="" class="responsive">
+      <img src="assets/img/STAYTUNED.svg" alt="" class="responsive">
+     
+    </div>
+  </section><!-- End Hero -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="content-footer ">
+        <div class="left">
+            <div class="flex" style="align-items:center">
+              <img src="assets/img/Logo oke.svg" alt="">
+
+                <!-- <h5 class="bt1" style="position: absolute ;  color: white;">Brawijaya Tournament</h5>
+                <h5 class="bt2" style="position: absolute ; color: #022051; margin-left: 2.9rem;" >Brawijaya Tournament</h5>
+                 -->
+                <img src="assets/img/bt-logo.svg" alt="" >
+            </div>
+            
+            <div class="credit">
+                <h6 style="font-family:poppins ; padding-top: 0.5rem;">
+                    © 2022 <b>Brawijaya Tournament. </b> All rights reserved.
+                </h6>
+            </div>
+        </div>
+        <div class="right">
+            <div class="moreinfo">
+              <h6 style="font-family:poppins ;">
+                    For more you can find us on
+                </h5>
+
+                <div>
+                    <a href="https://instagram.com/brawijayatournament?igshid=YmMyMTA2M2Y="><img src="assets/img/Logo Instagram.png" alt="" class="responsive"></a>
+                    <a href="https://twitter.com/BT_Brawijaya"> <img src="assets/img/Logo Twitter.png" alt="" class="responsive"></a>
+                    <a href="https://www.tiktok.com/@brawijayatournament?_t=8UZnAiIPghO&_r=1"><img src="assets/img/Logo Tiktok.png" alt="" class="responsive"></a>
+                    <a href="https://www.youtube.com/channel/UCpfEgh85R0TkWyBuGTDMGYw"><img src="assets/img/Logo Youtube.png" alt="" class="responsive"></a>
+                    <a href="http://line.me/ti/p/~640atdxb"><img src="assets/img/Logo Line.png" alt="" class="responsive"></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+   
+
+                
+  </footer><!-- End #footer -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
