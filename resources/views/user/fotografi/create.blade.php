4b23247dc0426e5fe586fb5aa8f2b5eaac0951e4
Nanda Safira <111990850+nandasafiraa@users.noreply.github.com>
2022-10-08 22:27:41 +0700

    create.blade.php
    pengkondisian error

--- resources/views/user/fotografi/create.blade.php
+++ resources/views/user/fotografi/create.blade.php
@@ -10,6 +10,12 @@
                         <i class="fa-solid fa-angle-left text-white text-2xl"></i>
                         <p class="text-white font-bold text-2xl px-3">PENDAFTARAN FOTOGRAFI</p>
                     </div>
+                    @if(session()->has('message'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                      {{ session('message') }}
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
                  <!-- form informasi tim -->
                  <form method="post" action="/fotografi/register" class="mb-5" enctype="multipart/form-data">
                     @csrf
@@ -25,7 +31,12 @@
                                 </label>
                                 <input type="text"
                                     class="appearance-none block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none "
-                                    id="grid-last-name place" placeholder="Masukkan asal universitas" name="universitas">
+                                    id="grid-last-name place" placeholder="Masukkan asal universitas" name="universitas" value="{{ old('universitas') }}">
+                                @error('universitas')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                     </div>
@@ -47,15 +58,25 @@
                                 </label>
                                 <input
                                     class="appearance-none block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none"
-                                    id="grid-first-name" type="text" placeholder="Masukkan nama" name="{{ "nama" . $i }}">
+                                    id="grid-first-name" type="text" placeholder="Masukkan nama" name="{{ "nama" . $i }}" value="{{ old('nama' . $i) }}">
+                                @error('nama' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <!-- No HandPhone -->
                             <div class="font-poppins text-secondary2 px-1 md:px-5 lg:px-2 lg:w-full">
                                 <label class="pace-x-9 w-11/12 block mb-2 text-sm font-medium text-secondary2 dark:text-secondary2"
                                     for="link_berkas">Nomor HandPhone</label>
-                                    <input type="text"
+                                <input type="text"
                                     class="appearance-none  block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none"
-                                    id="grid-last-name place" placeholder="Masukkan nomor handphone" name="{{ "hp" . $i }}">
+                                    id="grid-last-name place" placeholder="Masukkan nomor handphone" name="{{ "hp" . $i }}" value="{{ old('hp' . $i) }}">
+                                @error('hp' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <!-- row2 -->
@@ -70,7 +91,7 @@
                                 <div class="relative">
                                     <select
                                         class="w-full mb-2 bg-primary1 border border-secondary2 text-secondary2 py-2.5 px-4 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-primary1 focus:border-secondary2"
-                                        id="grid-state" name="{{ "gender" . $i }}">
+                                        id="grid-state" name="{{ "gender" . $i }}" value="{{ old('gender' . $i) }}">
                                         <option value="Laki-laki">Laki-laki</option>
                                         <option value="Perempuan">Perempuan</option>
                                     </select>
@@ -85,7 +106,12 @@
                                 </label>
                                 <input type="text"
                                     class="appearance-none block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none "
-                                    id="grid-last-name place" placeholder="Masukkan fakultas" name="{{ "fakultas" . $i }}">
+                                    id="grid-last-name place" placeholder="Masukkan fakultas" name="{{ "fakultas" . $i }}" value="{{ old('fakultas' . $i) }}">
+                                @error('fakultas' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <div class="md:flex justify-between">
@@ -98,7 +124,12 @@
                                 </label>
                                 <input
                                     class="appearance-none block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none"
-                                    id="grid-first-name" type="text" placeholder="Masukkan email" name="{{ "email" . $i }}">
+                                    id="grid-first-name" type="text" placeholder="Masukkan email" name="{{ "email" . $i }}" value="{{ old('email' . $i) }}">
+                                @error('email' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <!-- angkatan -->
                             <div class="font-poppins text-secondary2 px-1 md:px-5 lg:px-2 lg:w-full">
@@ -109,7 +140,12 @@
                                 </label>
                                 <input type="text"
                                     class="appearance-none  block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none"
-                                    id="grid-last-name place" placeholder="Masukkan angkatan" name="{{ "angkatan" . $i }}">
+                                    id="grid-last-name place" placeholder="Masukkan angkatan" name="{{ "angkatan" . $i }}" value="{{ old('angkatan' . $i) }}">
+                                @error('angkatan' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <div class="md:flex justify-between">
@@ -122,7 +158,12 @@
                                 </label>
                                 <input
                                     class="appearance-none block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none"
-                                    id="grid-first-name" value="" type="text" placeholder="Masukkan Nim" name="{{ "nim" . $i }}">
+                                    id="grid-first-name" value="" type="text" placeholder="Masukkan Nim" name="{{ "nim" . $i }}" value="{{ old('nim' . $i) }}">
+                                @error('nim' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <!-- berkas -->
                             <div class="font-poppins text-secondary2 px-1 md:px-5 lg:px-2 lg:w-full">
@@ -133,7 +174,12 @@
                                 </label>
                                 <input type="text"
                                     class="appearance-none block w-full bg-primary1 text-secondary2 border border-secondary2 rounded-lg py-3 px-4 mb-3 leading-5 focus:outline-none "
-                                    id="grid-last-name place" placeholder="Masukkan link gdrive" name="{{ "link_gdrive" . $i }}">
+                                    id="grid-last-name place" placeholder="Masukkan link gdrive" name="{{ "link_gdrive" . $i }}" value="{{ old('link_gdrive' . $i) }}">
+                                @error('link_gdrive' . $i)
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <br>
